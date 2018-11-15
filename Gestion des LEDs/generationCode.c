54906b550f0e8336fead81114b78113f37e9819a
Ypsillonn <35596935+Ypsillonn@users.noreply.github.com>
2018-11-15 11:52:08 +0100

    Commentaires generateur de code

--- Gestion des LEDs/generationCode.c
+++ Gestion des LEDs/generationCode.c
@@ -5,13 +5,13 @@
 
 bool ecriture(int mode, int x){
     char *modeLeds = NULL;
-    modeLeds = fopen("coeur/param.h","w+");
+    modeLeds = fopen("coeur/param.h","w+");     // On ouvre le fichier param.h pour écrire dessus en le vidant d'abord
 
-    if(modeLeds == NULL){
+    if(modeLeds == NULL){       // Si le fichier n'est pas trouvé
         printf("/nEchec d'ouverture du fichier. Fermeture./n");
         return false;
     }
-    fprintf(modeLeds,"#ifndef PARAM_H_\n#define PARAM_H_\n\nint mode = %d;\nint x = %d;\n\n#endif /* PARAM_H */", mode, x);
-    fclose(modeLeds);
+    fprintf(modeLeds,"#ifndef PARAM_H_\n#define PARAM_H_\n\nint mode = %d;\nint x = %d;\n\n#endif /* PARAM_H */", mode, x);     // On écrit dans param.h
+    fclose(modeLeds);       // On ferme le fichier
     return true;
 }
--- Gestion des LEDs/menu.c
+++ Gestion des LEDs/menu.c
@@ -12,31 +12,32 @@ int menu(){
     printf("(4) Chenille\n");
     printf("(5) Chenille avec clignotement\n");
     printf("(6) Une seule LED au choix\n\n");
-    scanf("%d", &mode);
-    while(mode>6 || mode<1){
+    scanf("%d", &mode);     // On fait choisir le mode à l'utilisateur
+
+    while(mode>6 || mode<1){        // Si l'utilisateur n'a pas rentré un bon numéro...
         printf("\nVeuillez entrez une valeur comprise entre 1 et 6.");
         scanf("%d", &mode);
     }
-    if(mode == 3){
+    if(mode == 3){     // On fait choisir x à l'utilisateur
         printf("\nCombien vaut X ? (entre 1 et 10)\n");
         scanf("%d",&x);
-        while(x>10 || x<1){
+        while(x>10 || x<1){        // Si l'utilisateur n'a pas rentré un bon numéro...
             printf("\nVeuillez entrez une valeur comprise entre 1 et 10.\n");
             scanf("%d", &x);
         }
     }
-    else if(mode == 6){
+    else if(mode == 6){     // On fait choisir la led à l'utilisateur
         printf("\nQuelle LED doit s'eclairer ?(entre 1 et 10)\n");
         scanf("%d",&x);
-        while(x>10 || x<1){
+        while(x>10 || x<1){        // Si l'utilisateur n'a pas rentré un bon numéro...
             printf("\nVeuillez entrez une valeur comprise entre 1 et 10.\n");
             scanf("%d", &x);
         }
     }
-    if (ecriture(mode, x)){
+    if (ecriture(mode, x)){         // On lance la fonction ecriture()
         printf("\nSucces de la generation du code. Voulez-vous modifier votre choix ? (1- oui, autre - non)\n\n");
         scanf("%d", &x);
-        if (x == 1){
+        if (x == 1){        // Si l'utilisateur veut il peut recommencer
             menu();
         }
     }
